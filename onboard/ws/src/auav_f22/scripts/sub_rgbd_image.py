8151d86a47b36e9f99a6e6bf6e6528770c64e94f
Jeremy233 <30543532+Jeremy233@users.noreply.github.com>
2022-10-23 04:23:00 -0400

    agent now generate file to given folder

--- onboard/ws/src/auav_f22/scripts/sub_rgbd_image.py
+++ onboard/ws/src/auav_f22/scripts/sub_rgbd_image.py
@@ -2,19 +2,45 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
+import time
 
 class RGBDImageNode(Node):
 
 	def __init__(self):
 		super().__init__("RGBDImage_subscriber")
 		self.RGBDImage_subscriber = self.create_subscription(RGBDImage, "/rgbd_camera/depth_image", self.sensor_callback, 10)
+		self.write_check = True
+		self.file_count = 0
+		self.rgbd_image = None
+
+	def write_file(self):
+		if self.write_check is True:
+			file_name = "./output/rgbd_image" + str(self.file_count) + ".txt"
+			f = open(file_name, "w")
+			for i in self.rgbd_image:
+				f.write(str(i) + '\n')
+			f.close
 
 	def sensor_callback(self, msg:RGBDImage):
-		self.get_logger().info(str(msg))
-		
+		self.rgbd_image = msg.data
+		# write data to txt file, stop when wrote 100 txts
+		while self.file_count > 0:
+			if self.file_count > 100:
+				self.file_count = -1
+			else:
+				self.write_file()
+				time.sleep(0.1)
+			self.file_count += 1
+			
+		self.get_logger().info("rgbd image wrote") # subscribe from the node
+
+
 
 def main(args=None):
 	rclpy.init(args=args)
-	node = RGBDImageNode()
-	rclpy.spin(node)
+	rgbd_image = RGBDImageNode()
+	rclpy.spin(rgbd_image)
 	rclpy.shutdown()
+
+if __name__ == '__main__':
+	main()
--- onboard/ws/src/auav_f22/scripts/sub_sensors.py
+++ onboard/ws/src/auav_f22/scripts/sub_sensors.py
@@ -2,21 +2,43 @@
 import rclpy
 from rclpy.node import Node
 from px4_msgs.msg import SensorCombined
+import time
 
 class SensorCombinedNode(Node):
 
 	def __init__(self):
 		super().__init__("sensor_subscriber")
-		self.sensor_subscriber = self.create_subscription(
-			SensorCombined, "/SensorCombined_PubSubTopic", 
-			self.sensor_callback, 10)
+		self.sensor_subscriber = self.create_subscription(SensorCombined, "/SensorCombined_PubSubTopic", self.sensor_callback, 10)
+		self.write_check = True
+		self.file_count = 0
+		self.sensor_data = None
+
+	def write_file(self):
+		if self.write_check is True:
+			file_name = "./output/sensor_comb" + str(self.file_count) + ".txt"
+			f = open(file_name, "w")
+			for i in self.sensor_comb:
+				f.write(str(i) + '\n')
+			f.close
 
 	def sensor_callback(self, msg:SensorCombined):
-		self.get_logger().info(str(msg))
-		
+		self.sensor_comb = msg.data
+		# write data to txt file, stop when wrote 100 txts
+		while self.file_count > 0:
+			if self.file_count > 100:
+				self.file_count = -1
+			else:
+				self.write_file()
+				time.sleep(0.1)
+			self.file_count += 1
+
+		self.get_logger().info("sensor wrote")
 
 def main(args=None):
 	rclpy.init(args=args)
 	node = SensorCombinedNode()
 	rclpy.spin(node)
 	rclpy.shutdown()
+
+if __name__ == '__main__':
+	main()
